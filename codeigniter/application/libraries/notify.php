91aa46bcfce2b5b21672981f82db590846f75f2b
antongorodezkiy <antongorodezkiy@gmail.com>
2012-07-12 17:38:52 +0300

    Fixed https redirects

--- codeigniter/application/libraries/notify.php
+++ codeigniter/application/libraries/notify.php
@@ -372,7 +372,7 @@ class Notify
 		{
 			if ($this->returnTo == '/')
 				$this->returnTo = base_url();
-			elseif(strstr($this->returnTo,'http://'))
+			elseif(strstr($this->returnTo,'http://') or strstr($this->returnTo,'https://'))
 				$this->returnTo = $this->returnTo;
 			else
 				$this->returnTo = site_url($this->returnTo);
