0a1345b79ea50333fc676060805a150605631666
s_les <dev@sattelite-dev.(none)>
2013-10-24 14:47:17 +0300

    Added jquery live legacy support

--- codeigniter/application/libraries/notify.php
+++ codeigniter/application/libraries/notify.php
@@ -14,10 +14,10 @@
 *
 * @package codeigniter-notify-library
 * @author Eduardo Kozachek <eduard.kozachek@gmail.com>
-* @version $Revision: 1.03 $
+* @version $Revision: 1.04 $
 * @access public
-* @see http://nadvoe.org.ua
-* @changed 25.05.12 19:21
+* @see http://eduard.kozachek.net
+* @changed 24.10.13 14:42
 */
 
 class Notify
@@ -169,6 +169,68 @@ class Notify
 	
 	
 	private $js = '
+	
+			(function($) {
+				/**
+				 * (c) http://stackoverflow.com/questions/2655308/jquery-version-compatibility-detection/3113707#3113707
+				 * Used for version test cases.
+				 * 
+				 * @param {string} left A string containing the version that will become
+				 *        the left hand operand.
+				 * @param {string} oper The comparison operator to test against. By
+				 *        default, the "==" operator will be used.
+				 * @param {string} right A string containing the version that will
+				 *        become the right hand operand. By default, the current jQuery
+				 *        version will be used.
+				 *        
+				 * @return {boolean} Returns the evaluation of the expression, either
+				 *         true or false.
+				 *
+				 *
+				 *	$.isVersion("1.4.2"); // returns true, if $().jquery == "1.4.2"
+				 *	$.isVersion("1.3.2", ">"); // returns true if $().jquery > "1.3.2"
+				 *	$.isVersion("1.3", ">", "1.2.6"); // returns true
+				 *	$.isVersion("1.3.2", "<", "1.3.1"); // returns false
+				 *	$.isVersion("1.4.0", ">=", "1.3.2"); // returns true
+				 *	$.isVersion("1.4.1", "<=", "1.4.1"); // returns true
+				 *         
+				 */
+				$.isVersion = function(left, oper, right) {
+					if (left) {
+						var pre = /pre/i,
+							replace = /[^\d]+/g,
+							oper = oper || "==",
+							right = right || $().jquery,
+							l = left.replace(replace, ""),
+							r = right.replace(replace, ""),
+							l_len = l.length, r_len = r.length,
+							l_pre = pre.test(left), r_pre = pre.test(right);
+			
+						l = (r_len > l_len ? parseInt(l) * ((r_len - l_len) * 10) : parseInt(l));
+						r = (l_len > r_len ? parseInt(r) * ((l_len - r_len) * 10) : parseInt(r));
+			
+						switch(oper) {
+							case "==": {
+								return (true === (l == r && (l_pre == r_pre)));
+							}
+							case ">=": {
+								return (true === (l >= r && (!l_pre || l_pre == r_pre)));
+							}
+							case "<=": {
+								return (true === (l <= r && (!r_pre || r_pre == l_pre)));
+							}
+							case ">": {
+								return (true === (l > r || (l == r && r_pre)));
+							}
+							case "<": {
+								return (true === (l < r || (l == r && l_pre)));
+							}
+						}
+					}
+			
+					return false;
+				}
+			})(jQuery);
 		
 			function notify(json, globalTtl, globalRegion)
 			{
@@ -270,7 +332,12 @@ class Notify
 			
 			$(document).ready(function()
 			{
-				$(".notice",".notify").live("click",function(){ $(this).fadeOut(300); });
+				if ($.isVersion("1.7", ">")) {
+					$(".notice",".notify").live("click",function(){ $(this).fadeOut(300); });
+				}
+				else {
+					$(".notify").on("click",".notice",function(){ $(this).fadeOut(300); });
+				}
 				
 				setInterval("close_old_notifies()",500);
 				
